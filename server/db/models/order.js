ce5fd896be1b08ad2e1e7ef3d5a2be461280739f
Beth Barnes <bethcbarnes@outlook.com>
2018-02-26 17:08:40 -0500

    testing waffle resolve #1

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,7 +4,7 @@ const db = require('../db')
 const Order = db.define('order', {
 
   productsOrdered: {
-    type: Sequelize.ARRAY,  //planning to have array of objects with product id and price at time of order
+    type: Sequelize.ARRAY(Sequelize.TEXT),  //planning to have array of objects with product id and price at time of order
   }
 
 })
