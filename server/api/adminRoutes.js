6702f5fd2b6d9db0f9101554becbe240bdc45b7f
Jeffrey Goldbeck <jeffg5001@gmail.com>
2018-02-28 12:57:58 -0500

    fixed an error that admin route was not a function

--- server/api/adminRoutes.js
+++ server/api/adminRoutes.js
@@ -1,12 +1,12 @@
-// const router = require('express').Router()
-// const { Products } = require('../db/models')
-// module.exports = router
+const router = require('express').Router()
+const { Products } = require('../db/models')
+module.exports = router
 
-// router.delete('/:id', (req, res, next) => {
-//   const deleteId = req.params.id;
-//   Products.destroy({ where: { id: deleteId} })
-//     .then(() => res.sendStatus(204))
-//     .catch(next)
-// })
+router.delete('/:id', (req, res, next) => {
+  const deleteId = req.params.id;
+  Products.destroy({ where: { id: deleteId} })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
 
-//TODO created a delete products route to use for admins
+// TODO created a delete products route to use for admins
