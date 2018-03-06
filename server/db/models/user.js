b7130b19dac44060055654a77b781694b89c3fa7
Beth Barnes <bethcbarnes@outlook.com>
2018-03-06 11:11:25 -0500

    Update user.js
    deleted the thing

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -40,10 +40,6 @@ const User = db.define('user', {
   lastName: {
     type: Sequelize.STRING
   },
-  currentCart: {
-    type: Sequelize.JSON,
-    defaultValue: '[]'
-  }
 
 }, {
   getterMethods: {
